d69ff1b6c2e378d4000b7f2e69e5132c98adb2ee
DNA5769 <dennisthomas2002@gmail.com>
2021-04-03 01:16:01 +0530

    Implemented updating tasks

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -8,6 +8,8 @@ class App extends Component {
     this.state = {
       title: '',
       todos: [],
+      editing: false,
+      editingID: null
     };
   }
 
@@ -28,24 +30,48 @@ class App extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
 
-    fetch('http://localhost:8000/api/create-task/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ title: this.state.title })
-    }).then(response => response.json()).then(data => this.setState(state => {
-      return {
-        title: '',
-        todos: [...state.todos, data],
-      };
-    }));
+    if (!this.state.editing)
+    {  
+      fetch('http://localhost:8000/api/create-task/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ title: this.state.title })
+      }).then(response => response.json()).then(data => this.setState(state => {
+        return {
+          title: '',
+          todos: [...state.todos, data],
+        };
+      }));
+    }
+    else
+    {
+      fetch(`http://localhost:8000/api/update-task/${this.state.editingID}/`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ title: this.state.title })
+      }).then(response => response.json()).then(data => {
+        this.setState({ title: '', editing: false, editingID: null });
+        this.componentDidMount();
+      });
+    }
   }
 
   handleDelete = id => {
     fetch(`http://localhost:8000/api/delete-task/${id}/`, { method: 'DELETE' })
       .then(response => response.json())
       .then(data => this.componentDidMount());
+  }
+
+  handleUpdate = (id, title) => {
+    this.setState({
+      title: title,
+      editing: true,
+      editingID: id,
+    });
   }
 
   render() {
@@ -60,6 +86,7 @@ class App extends Component {
             <div>
               <h1>{todo.title}</h1>
               <button onClick={() => self.handleDelete(todo.id)}>Delete</button>
+              <button onClick={() => self.handleUpdate(todo.id, todo.title)}>Update</button>
             </div>
           );
         })}
