0ba06754b7071e23a51e28fb58631741cbfc2d31
DNA5769 <dennisthomas2002@gmail.com>
2021-04-02 19:05:37 +0530

    Created /api/create-task route

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Task
+from .serializers import TaskSerializer
 
 # Create your views here.
 def index(req):
     return HttpResponse('<h1>Hello World</h1>')
+
+@api_view(['POST'])
+def createTask(req):
+    serializer = TaskSerializer(data=req.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
