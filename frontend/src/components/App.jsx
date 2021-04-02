d6fabf51a65605460e331ace308e217b18413bf5
DNA5769 <dennisthomas2002@gmail.com>
2021-04-02 23:32:27 +0530

    Basic form input and styling in frontend

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import './App.css'
 
 class App extends Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      title: '',
+      todos: [],
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      'title': e.target.value
+    });
+  }
+
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
+      <div className="box">
+        <form>
+          <input onChange={this.handleChange} type="text" name="title" value={this.state.title}/>
+        </form>
       </div>
     );
   }
